d1e1de577b828074fae6aac86bda9202c973a7da
houseme <qiuzhigang@iboxpay.com>
2017-03-30 16:21:18 +0800

    修改

--- example/wxpayTest.php
+++ example/wxpayTest.php
@@ -11,22 +11,22 @@ namespace houseme\wxpaytest;
  */
 //use houseme\wxpay\WxPayConfig as WxPayConfig;
 
-use houseme\wxpay\wxpay;
+use houseme\wxpay\wxPay;
 
 //use houseme\wxpay\WxPayConfig;
 
-include "../src/lib/WxPayConfig.class.php";
-include "../src/wxpay.php";
+//include "../src/WxPayConfig.class.php";
+include "../src/WxPay.class.php";
 $appId       = '1';
 $mchId       = '2';
 $key         = '3';
 $appSecret   = '4';
 $sslCertPath = '111/32.txt';
 $sslKeyPath  = '123';
-
+wxPay::register_autoloader();
 //$wxPayConfig = new WxPayConfig($appId, $mchId, $key, $appSecret, $sslCertPath, $sslKeyPath);
-$wxPay       = new wxpay($appId, $mchId, $key, $appSecret, $sslCertPath, $sslKeyPath);
-$wxPayConfig = wxpay::getWxPayConfig();
+$wxPay       = new wxPay($appId, $mchId, $key, $appSecret, $sslCertPath, $sslKeyPath);
+$wxPayConfig = wxPay::getWxPayConfig();
 var_dump($wxPayConfig::getAppId());
 
 $wxPayConfig::setAppId(123);
